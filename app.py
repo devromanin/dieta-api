415ec95bfde2560d72e094e2e1186167f30f8ed2
Kevin Romanin <kevinromanin145@gmail.com>
2025-12-15 20:02:10 -0300

    configuração correta do SQLAlchemy com .env

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from database import db
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
+db.init_app(app)
 
